073d3a51ee31923d2e28ba33c1e128c26ded27e1
vera <ruvinskaya@gmail.com>
2015-10-02 00:26:26 +0200

    Openshift integration fix

--- wp-content/themes/accelerate-theme/front-page.php
+++ wp-content/themes/accelerate-theme/front-page.php
@@ -13,13 +13,12 @@
  */
 
 get_header(); ?>
-
 <section class="home-page">
 	<div class="site-content">
 		<?php while ( have_posts() ) : the_post(); ?>
 			<div class='homepage-hero'>
 				<?php the_content(); ?>
-				<a class="button" href="<?php echo home_url(); ?>/blog">View Our Work</a>
+				<a class="button" href="<?php echo site_url(); ?>/blog">View Our Work</a>
 			</div>
 		<?php endwhile; // end of the loop. ?>
 	</div><!-- .container -->
